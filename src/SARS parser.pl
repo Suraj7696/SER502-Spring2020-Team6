c9cba38a5a05f9f6b40ca33adc140acc7e83bf50
Suraj Atmakuri <surajatmakuri@hotmail.com>
2020-04-21 22:14:37 -0700

    added changes to grammar

--- src/SARS parser.pl
+++ src/SARS parser.pl
@@ -6,22 +6,18 @@ block_part --> command, block_part.
 block_part --> command.
 
 command --> declaration,[;].
+command --> while.
 command --> assignment,[;].
-command -->expression,[;].
-command -->bool,[;].
 command -->output,[;].
 command -->if.
 command --> ternary,[;].
 command -->for.
-command -->while.
 command -->for_range.
+command --> iterator, [;].
 
-bool --> [true];[false].
-bool --> expression, condition_operator, expression.
-bool --> [not],['('],bool,[')'].
-bool --> bool, [and], bool.
-bool --> bool, [or], bool.
-bool --> condition.
+type --> [int].
+type --> [string].
+type --> [bool].
 
 declaration --> [int], id, [=], num.
 declaration --> [string], id, [=], string.
@@ -33,10 +29,25 @@ assignment --> id, [=], expression.
 assignment --> id, [=], string.
 assignment --> id, [=], bool.
 
-type --> [int];[string],[bool].
+:- table bool/2.
+bool --> [true].
+bool --> [false].
+bool --> [not],['('],bool,[')'].
+bool --> bool, [and], bool.
+bool --> bool, [or], bool.
+bool --> condition.
 
-for --> [for],['('],assignment,condition,iterator,[')'],block.
-for --> [for],['('],assignment,condition,expression,[')'],block.
+condition --> expression, condition_operator, expression.
+condition --> string, condition_operator, string.
+condition --> id, condition_operator, string.
+
+condition_operator --> [==];['!='];['>'];['<'];['>='];['<='].
+
+
+
+
+for --> [for],['('],assignment,[;],condition,[;],iterator,[;],[')'],block.
+for --> [for],['('],assignment,[;],condition,[;],expression,[;],[')'],block.
 
 while --> [while], ['('], condition,[')'], block.
 
@@ -55,12 +66,9 @@ output --> [print], ['('], id,[')'].
 output --> [print], ['('], num,[')'].
 output --> [print], ['('], string,[')'].
 
-condition --> expression, condition_operator, expression.
-condition --> string, condition_operator, string.
-condition --> id, condition_operator, string.
 
-condition_operator --> [==];['!='];['>'];['<'];['>='];['<='].
 
+:- table expression/2,term/2.
 expression --> expression, [+], term.
 expression --> expression, [-], term.
 expression --> term.
@@ -72,14 +80,17 @@ term --> num; id.
 iterator --> id, [++].
 iterator --> id, [--].
 
+:- table num/2.
 num --> num,digit.
 num --> digit.
 digit --> [0];[1];[2];[3];[4];[5];[6];[7];[8];[9].
 
 id --> lowerchar, id.
 id --> num, id.
+id --> num;lowerchar.
 
-string --> ["\""],openstring,["\""].
+:- table openstring/2, letterstring/2.
+string --> ['\"'],openstring,['\"'].
 openstring --> openstring,letterstring.
 openstring --> openstring,num.
 openstring --> [].
@@ -93,17 +104,3 @@ lowerchar --> [a];[b];[c];[d];[e];[f];[g];[h];[i];
 upperchar --> ['A'];['B'];['C'];['D'];['E'];['F'];['G'];['H'];['I'];['J']
     ;['K'];['L'];['M'];['N'];['O'];['P'];['Q'];['R'];['S'];['T'];['U'];['V'];
     ['W'];['X'];['Y'];['Z'].
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
